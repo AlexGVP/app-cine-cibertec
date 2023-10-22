1bc68842ea6a825d8dee0973649591d51f56bc1d
AlexGVP <i202122156@cibertec.edu.pe>
2023-10-22 09:41:08 -0500

    mensaje

---
+++ src/main/java/cibertec/edu/pe/appcinecibertec/controller/HomeController.java
@@ -0,0 +1,14 @@
+package cibertec.edu.pe.appcinecibertec.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String index(Model model) {
+        return "home";
+    }
+}
