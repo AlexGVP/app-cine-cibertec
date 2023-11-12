4f13754c3b0d557f0d64e9275288a0c257666066
AlexGVP <i202122156@cibertec.edu.pe>
2023-11-12 12:22:26 -0500

    mensaje

--- src/main/resources/static/scripts/web/administracion/frmsala.js
+++ src/main/resources/static/scripts/web/administracion/frmsala.js
@@ -16,3 +16,22 @@ $(document).on("click", "#btnnuevo", function(){
             });
     $("#modalsala").modal("show");
 });
+
+$(document).on("click", ".btnactualizar", function(){
+    $("#modalsala").modal("show");
+    $("#txtdescripcion").val($(this).attr("data-descsala"));
+    $("#txtasientos").val($(this).attr("data-asientos"));
+    $("#cboestado").empty($(this).attr("data-idestado"));
+    $.ajax({
+                    type: "GET",
+                    url: "/administracion/estado/listar",
+                    dataType: "json",
+                    success: function(resultado){
+                        $.each(resultado, function(index, value){
+                        $("#cboestado").append(
+                        `<option value="${value.idestado}">${value.descestado}</option>`)
+                        });
+                    }
+                });
+    $("#hddidestado").val($(this).attr("data-idsala"));
+});
